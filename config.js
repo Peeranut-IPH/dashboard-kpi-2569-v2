b3994a584c47dc12e1d1336281cfcb6e5fa305a0
Peeranut-IPH <iph.peeranut@gmail.com>
2026-07-17 14:02:15 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzABotJjzkqrKGAyO93YU5_k2aDwbfKYLRH76PtENLh8126jwJsWiOE48j8YkCJtYS9hw/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxsNG0pUP8uKkaMQAy_auH_F9JvBw9lJoclOE8OoVPfrIxYH4Q-23b2jKqoyk3eAK0yzw/exec",
   APP_NAME: "ระบบวิเคราะห์ตัวชี้วัด",
   APP_SUBTITLE: "แผนปฏิบัติราชการ โรงพยาบาลกลาง ปีงบประมาณ 2569",
   REFRESH_SECONDS: 30,
